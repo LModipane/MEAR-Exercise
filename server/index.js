57120b6ece71a02e2026b6429e24ef18172f48e4
shaun <modipanesh@gmail.com>
2022-09-05 08:28:33 +0200

    created routes

--- server/index.js
+++ server/index.js
@@ -6,14 +6,17 @@ import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
 
+import memoryRoutes from "./routes/memories.js"
+
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use("/memories", memoryRoutes);
 const PORT = process.env.PORT || 3000;
 
-const start = async () => {
+const startServer = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URL);
         app.listen(PORT, () => { console.log(`Server listening at port ${PORT}....`) });
@@ -23,4 +26,4 @@ const start = async () => {
     }
 };
  
-start();
+startServer();
