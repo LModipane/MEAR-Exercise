dad8c5b1eb8f89e12609e3492d5955486d232bc3
shaun <modipanesh@gmail.com>
2022-09-05 08:06:32 +0200

    connected server side to mongoDB database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        app.listen(PORT, () => { console.log(`Server listening at port ${PORT}....`) });
+
+    } catch (error) {
+        console.log(error)
+    }
+};
+ 
+start();
